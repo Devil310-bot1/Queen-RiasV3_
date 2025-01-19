f51215b6cb35c5a65f82e10ea5339a6233a00484
Devil310-bot1 <rowanbassett8@gmail.com>
2025-01-19 11:06:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 require("dotenv").config(); // Load environment variables
 
 module.exports = {
-    prefix: process.env.PREFIX || ".", // Command prefix
+    prefix: process.env.PREFIX || "!", // Command prefix
 
-    ownerName: process.env.OWNER_NAME || "Toxxic-Boy", // Owner name
+    ownerName: process.env.OWNER_NAME || "Devil", // Owner name
 
-    ownerNumber: process.env.OWNER_NUMBER || "2348165846414", // Your WhatsApp number
+    ownerNumber: process.env.OWNER_NUMBER || "491786327165", // Your WhatsApp number
 
     mode: process.env.MODE || "public", // Bot mode: 'public' or 'private'
 
     region: process.env.REGION || "Nigeria", // Region
 
-    botName: process.env.BOT_NAME || "Rias Gremory V3", // Bot name
+    botName: process.env.BOT_NAME || "Devils V3", // Bot name
 
     exifPack: process.env.EXIF_PACK || "RIAS V3 LOVES", // Sticker pack name
 
